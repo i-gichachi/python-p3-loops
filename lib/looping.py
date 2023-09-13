a60bdd9013d5543233dbb45aebf6cad886c83f0d
Ian Mwaura Gichachi <iangichachi@gmail.com>
2023-09-13 18:44:59 +0300

    Completed Assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    counter=10
+    while counter >= 1:
+        print(counter)
+        counter -= 1
+        print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = [num ** 2 for num in int_list]
+    return squared_list
+pass
 
 def fizzbuzz():
-    # code goes here!
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
     pass
